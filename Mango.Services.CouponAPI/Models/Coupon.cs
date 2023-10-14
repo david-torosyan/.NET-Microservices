efae99aac9b152b38c1603463cb372fd1c4d17dc
Torosyan David <davit.torosyan.0214@gmail.com>
2023-10-14 20:03:00 +0400

    Add project files.

---
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.CouponAPI.Models
+{
+    public class Coupon
+    {
+
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
