d2a1a742a06a670a0accb8a70f29e2057e831104
Torosyan David <davit.torosyan.0214@gmail.com>
2023-10-14 22:16:54 +0400

    Added RequestModel and Enum for request types

---
+++ Mango.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET = 0,
+            POST = 1, 
+            PUT = 2,
+            DELETE = 3,
+        }
+    }
+}
