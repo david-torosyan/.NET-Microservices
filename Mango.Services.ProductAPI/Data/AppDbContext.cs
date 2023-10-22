b36693dd721b9467de56612e88e101422e7c3998
David Torosyan <138628225+david-torosyan@users.noreply.github.com>
2023-10-22 16:14:47 +0400

    Update AppDbContext.cs
    Shopping Api Response fixed

--- Mango.Services.ProductAPI/Data/AppDbContext.cs
+++ Mango.Services.ProductAPI/Data/AppDbContext.cs
@@ -9,6 +9,7 @@ namespace Mango.Services.ProductAPI.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
         public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
