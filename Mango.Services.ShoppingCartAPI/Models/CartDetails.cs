504d4a9cfb446335b60f1d440e5afb24071808dd
Torosyan David <davit.torosyan.0214@gmail.com>
2023-10-22 09:37:05 +0400

    Created ShoopingCartApi And modified

---
+++ Mango.Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        [Key]
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set; }
+        public int ProductId { get; set; }
+
+        [NotMapped]
+        // We are not adding refferens to ProductApi beacuse all microservices mus be isolated  
+        public ProductDto Product { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Models/CartHeader.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartHeader
+    {
+        [Key]
+        public int CartHeaderId { get; set; }
+        public string? UserId { get; set;}
+        public string? CouponCode { get; set;}
+
+        // Do not add in database because it is working dynamicly
+        [NotMapped]
+        public double Dicount { get; set;}
+        [NotMapped]
+        public double CartTotal { get; set;}
+    }
+}
